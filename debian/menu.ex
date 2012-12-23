850b41d1e3ea2c7971aff69b312ed1e02816a0bc
Devon Kearns <dookie@kali.org>
2012-12-23 12:29:56 -0700

    Commit before modifying

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(truecrack):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="truecrack" command="/usr/bin/truecrack"
---
+++ debian/truecrack.default.ex
@@ -0,0 +1,10 @@
+# Defaults for truecrack initscript
+# sourced by /etc/init.d/truecrack
+# installed at /etc/default/truecrack by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
