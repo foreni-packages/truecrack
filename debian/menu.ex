127e68f1b13eda0c20481ac149c0ad91fb59d64c
Devon Kearns <dookie@kali.org>
2012-12-23 12:45:13 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(truecrack):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="truecrack" command="/usr/bin/truecrack"
--- debian/truecrack.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the truecrack package
-#
-0 4	* * *	root	[ -x /usr/bin/truecrack_maintenance ] && /usr/bin/truecrack_maintenance
